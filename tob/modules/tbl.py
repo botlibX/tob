6ae9e3bd21c4e80e9a84f6707da63deecb7a7cf6
Bart Thate <bthate@dds.nl>
2025-09-25 01:16:33 +0200

    initial import

---
+++ tob/modules/tbl.py
@@ -0,0 +1,58 @@
+# This file is placed in the Public Domain.
+
+
+"lookup tables"
+
+
+NAMES = {
+    "cfg": "irc",
+    "dis": "mdl",
+    "dne": "tdo",
+    "dpl": "rss",
+    "eml": "mbx",
+    "exp": "rss",
+    "flt": "flt",
+    "fnd": "fnd",
+    "imp": "rss",
+    "log": "log",
+    "ls": "lst",
+    "mbx": "mbx",
+    "mre": "irc",
+    "nme": "rss",
+    "now": "mdl",
+    "pwd": "irc",
+    "rem": "rss",
+    "req": "req",
+    "res": "rss",
+    "rss": "rss",
+    "slg": "slg",
+    "syn": "rss",
+    "tdo": "tdo",
+    "thr": "thr",
+    "tmr": "tmr",
+    "udp": "udp",
+    "upt": "upt",
+    "wsd": "wsd"
+}
+
+MD5 = {
+    "flt": "123a0a6dbd005fdfa30466e121453bfd",
+    "fnd": "c295c5bd0ccb6916593f60e9099f76a1",
+    "irc": "55c554d83c95a1eba41c95930a53d7af",
+    "log": "fad2dc46758daa45c44354eee63041b5",
+    "lst": "7048f50ce66ab547552b4ee4fd250132",
+    "mbx": "3b19a2629870964e2d80948d8b9f4d5b",
+    "mdl": "03d6a25b1c7434d105a56e1cda8f9150",
+    "req": "793704d64443c4f19512924e294943ea",
+    "rss": "0016b3801d07d19504b1ffac0c96d49d",
+    "rst": "49c14f682f89343066b3283545c1cae0",
+    "slg": "0b71453a0d4dd3b21618c7df3c8cd6a4",
+    "tbl": "d41d8cd98f00b204e9800998ecf8427e",
+    "tdo": "045fb38dfcf5154d64a29457b92e6b05",
+    "thr": "bb2ec6fc78468d320b2354499dff2e74",
+    "tmr": "c623a4f766cbac06484e52ae100fff6e",
+    "udp": "76bcb370915e4d42f77eadb0115a95fa",
+    "upt": "bb9b9d8d3bf62381a185ff7115db1c00",
+    "web": "79dc711b7c8d22ac0eb63bebe90cd762",
+    "wsd": "321d13c64c8d6d8b18df6ae900b6374a",
+}
