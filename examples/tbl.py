8b1813b5cacb6dfcd41d03a5b53a7d01e11c9662
Bart Thate <bthate@dds.nl>
2025-10-28 06:12:46 +0100

    add script

---
+++ examples/tbl.py
@@ -0,0 +1,68 @@
+# This file is placed in the Public Domain.
+
+
+"lookup tables"
+
+
+NAMES = {
+    "cfg": "mods.irc",
+    "dis": "mods.mdl",
+    "dne": "mods.tdo",
+    "dpl": "mods.rss",
+    "eml": "mods.mbx",
+    "exp": "mods.rss",
+    "flt": "mods.flt",
+    "fnd": "mods.fnd",
+    "imp": "mods.rss",
+    "log": "mods.log",
+    "lou": "mods.sil",
+    "lst": "mods.lst",
+    "man": "mods.man",
+    "mbx": "mods.mbx",
+    "mod": "mods.mod",
+    "mre": "mods.irc",
+    "nme": "mods.rss",
+    "now": "mods.mdl",
+    "pth": "mods.pth",
+    "pwd": "mods.irc",
+    "rem": "mods.rss",
+    "req": "mods.req",
+    "res": "mods.rss",
+    "rss": "mods.rss",
+    "sil": "mods.sil",
+    "slg": "mods.slg",
+    "syn": "mods.rss",
+    "tdo": "mods.tdo",
+    "thr": "mods.thr",
+    "tmr": "mods.tmr",
+    "udp": "mods.udp",
+    "upt": "mods.upt",
+    "wsd": "mods.wsd"
+}
+
+
+MD5 = {
+    "flt": "9eb24fe56e70abb7a3e6df99fac355a2",
+    "fnd": "2e98d5ee35cd418f224c36315376cd30",
+    "irc": "0c31c6f79c73224df7a5910d87f2b72e",
+    "log": "fa76611b03fd54ee19da2ecd80a35034",
+    "lst": "580451b7fc5dd3eef045d3cd45f400e6",
+    "man": "44d64c5b78ccdfd02c76c9f6146fec9a",
+    "mbx": "41ffc5fdf6c9dc461926861a2de6a1c5",
+    "mdl": "7a3b07c446e950215d6ba65968a015f8",
+    "mod": "f210bc6da2c2c878d67b7cd345a9e9f6",
+    "pth": "11c7ca625f7d18a702b7249ab0452b8e",
+    "req": "bc1984d2e9de0310dc1b468f25c7ab8c",
+    "rss": "9fe037b11dcbeb8f1abc926882ef640c",
+    "rst": "a2acc52f6a6e2907f60d636a81f81a2b",
+    "sil": "b5258378bbe4d417b1336f0b22af64e5",
+    "slg": "0b71453a0d4dd3b21618c7df3c8cd6a4",
+    "tbl": "d41d8cd98f00b204e9800998ecf8427e",
+    "tdo": "201208d5076e9af6438b0ecd847e233e",
+    "thr": "f41a365c5e1cb432b580835e7ee07280",
+    "tmr": "d13dcc3df61e9044524679bbcdff180b",
+    "udp": "7c20b09ca8e52b7362a09ad74429a9d0",
+    "upt": "e9dcc2f8c967adf53ad6441543eeb51c",
+    "web": "6801dbaf3f1896c78d6c6ad0daa0f242",
+    "wsd": "002f0791c98cc75ed2e161d1b5d2998d",
+}
